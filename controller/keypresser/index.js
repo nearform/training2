441cc942c5c9bd5621de4badbc84d80111f53891
Michael Beswetherick <mbeswetherick@gmail.com>
2016-05-18 11:50:57 -0400

    we f*%&ing did it

--- controller/keypresser/index.js
+++ controller/keypresser/index.js
@@ -8,15 +8,26 @@ process.stdin.resume()
 keypress(process.stdin)
 
 module.exports = () => {
-  const keypresser = through()
+    const keypresser = through()
 
-  return keypresser
+    // listen for the "keypress" event
+    process.stdin.on('keypress', pushKey);
+
+    eos(keypresser, function () {
+        process.stdin.removeListener('keypress', pushKey);
+    })
+
+    function pushKey (ch, key) {
+        keypresser.push({key: key, ch: ch});
+    }
+
+    return keypresser
 }
 
 //tips:
 
-// see http://npm.im/keypress 
+// see http://npm.im/keypress
 // push objects onto stream with form {key, ch}
 // (literally... `push`)
-// use http://npm.im/end-of-stream to determine 
+// use http://npm.im/end-of-stream to determine
 // when stream has closed (for cleanup)
