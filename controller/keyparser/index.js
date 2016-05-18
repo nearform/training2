5836beb13fb49931c713a0a779605f0ad8abcc7c
Isaac White <isaac.white@nytimes.com>
2016-05-18 11:50:08 -0400

    Working keyparser

--- controller/keyparser/index.js
+++ controller/keyparser/index.js
@@ -1,10 +1,34 @@
 'use strict'
 
 const through = require('through2').obj
+const keyMap = {
+  ctrlc: 'die',
+  c: 'reset',
+  b: 'toggle-backled',
+  up: 'roll',
+  down: 'stop',
+  ',': 'slower',
+  '.': 'faster',
+  left: 'left',
+  right: 'right'
+}
 
-module.exports = () => through(function(keypress, enc, cb) {
-  
+module.exports = () => through(function (keyObj, enc, cb) {
+  const input = keyObj.key ? combineModifier(keyObj.key) : keyObj.ch
+  const name = keyMap[input]
+
+  if (!name) {
+    return cb()
+  }
+
+  // tips:
+  // output objects with form {name}
+  this.push({name})
+  cb()
 })
 
-// tips:
-// output objects with form {name}
+function combineModifier (key) {
+  const name = key.ctrl ? 'ctrl' : ''
+
+  return name + key.name
+}
