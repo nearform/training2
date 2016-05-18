5913e904708b08d62080cbb9404720122a0b673f
Isaac White <isaac.white@nytimes.com>
2016-05-18 14:38:23 -0400

    gamepad parser

--- controller/gamepadParser/index.js
+++ controller/gamepadParser/index.js
@@ -1,20 +1,20 @@
 'use strict'
 
 const through = require('through2').obj
-
-var gamepadParser = () => through(function(btn, enc, done) {
-  var cmd = {name: 'noop'}
-  if (btn.name === 'start') { cmd.name = 'reset' }
-  if (btn.name === 'select') { cmd.name = 'toggle-backled' }
-  if (btn.name === 'right') { cmd.name = 'right' }
-  if (btn.name === 'left') { cmd.name = 'left' }
-  if (btn.name === 'up') { cmd.name = 'roll' }
-  if (btn.name === 'down') { cmd.name = '180' }
-  if (btn.name === 'release') { cmd.name = 'stop' }
-  if (btn.name === 'a') { cmd.name = 'faster' }
-  if (btn.name === 'b') { cmd.name = 'slower' }  
-  
-  done(null, cmd)
+const buttonMap = {
+  start: 'reset',
+  select: 'toggle-backled',
+  right: 'right',
+  left: 'left',
+  up: 'roll',
+  down: '180',
+  release: 'stop',
+  a: 'faster',
+  b: 'slower'
+}
+var gamepadParser = () => through(function (btn, enc, done) {
+  const name = buttonMap[btn.name] || 'noop'
+  done(null, {name})
 })
 
 module.exports = gamepadParser
